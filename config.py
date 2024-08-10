bac276c7e9bb75ce0b2a3ada36b957e439b7a938
sungjinw04 <abhishekpandit4224@gmail.com>
2024-08-10 23:05:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,8 +47,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Team_Hunter_X")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Devils_Hell_0")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/astheticdpsforu")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ANG_COMMUNITY_7")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
@@ -112,13 +112,13 @@ PING_IMG_URL = getenv(
 PLAYLIST_IMG_URL = "https://telegra.ph/file/cbb216bd1e84c83bce233.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/51badb4aba8d2b7534604.mp4"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/bd3be91cf02a8e4373894.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/f1e2fce89f46e84e46207.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/f1e2fce89f46e84e46207.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/f1e2fce89f46e84e46207.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/f1e2fce89f46e84e46207.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/f1e2fce89f46e84e46207.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/f1e2fce89f46e84e46207.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/f1e2fce89f46e84e46207.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/7435ac7c4251779d59c71.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/d3d80cd8cc0a6363eb21d.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/37ffadfde57a375a786db.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/37ffadfde57a375a786db.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/59d7eaec62d37a98d71ec.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/3c550e5585607aeb25e1b.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/d3d80cd8cc0a6363eb21d.jpg"
 
 
 def time_to_seconds(time):
